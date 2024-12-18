b857fc85a62cc2b12d4113bdc611137af69a5d6b
scorpiontai <scorpiontai22@outlook.com>
2024-12-18 07:07:17 +0700

    perubahan pada link href verifikasi mail

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,11 +4,13 @@ import { users } from 'src/models/users.models';
 import * as argon2 from 'argon2'
 import { RandomcodeService } from 'src/randomcode/randomcode.service';
 import { NodemailerService } from 'src/nodemailer/nodemailer.service';
+import { RedisService } from 'src/redis/redis.service';
 
 @Injectable()
 export class UsersService {
     constructor(private readonly randomCode: RandomcodeService,
-        private readonly mailserv: NodemailerService
+        private readonly mailserv: NodemailerService,
+        private readonly redis: RedisService
     ) { }
     async signup(username: string, password: string, email: string): Promise<any> {
         try {
@@ -39,7 +41,9 @@ export class UsersService {
     async sendVerifyMessage(target: string): Promise<any> {
         try {
             const randomCoded = await this.randomCode.generateRandom()
-            await this.mailserv.sendMessage(target, `verifikasi`, `verifikasi link`, `<a href="http://localhost:4000/verify/email?code=${randomCoded}"`)
+            await this.redis.setWithTTL(`${target}:verifyCode`, randomCoded, 5 * 6000)
+            const redisVerify = await this.redis.get(`${target}:verifyCode`)
+            await this.mailserv.sendMessage(target, `verifikasi`, `verifikasi link`, `<a href="http://localhost:4000/verify/email?code=${randomCoded}&email=${target}"`)
             return `sukses mengirim kode verifikasi`
         } catch (err) {
             console.error(err.message);
