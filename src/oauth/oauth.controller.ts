9a32b60ab7ab44329ade35be1f00801961cc67c1
scorpiontai <scorpiontai22@outlook.com>
2024-12-19 20:26:53 +0700

    perubahan logic tentang penanganan kasus store user info o auth2 google

--- src/oauth/oauth.controller.ts
+++ src/oauth/oauth.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, UseGuards, Get, Post, Req, Res, Logger } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { users } from 'src/models/users.models';
 
 @Controller('oauth')
 export class OauthController {
@@ -18,13 +19,29 @@ export class OauthController {
     @UseGuards(AuthGuard('google'))
     async googleAuthRedirect(@Req() req, @Res() res): Promise<any> {
         try {
-            const { accessToken } = req.user
+            const { accessToken, name, lastName, email } = req.user
 
+            let message
             if (!accessToken)
                 return res.status(400).json({ message: "token tidak ditemukan, harap coba ulangi atau hubungi developer" })
 
-            res.setHeader("token", accessToken)
-            return res.status(200).json({ message: "success sign" })
+            //jika berhasil, maka buat akun
+            const fullName = `${name} ${lastName}`
+            const find = await users.findOne({ where: { username: fullName }, raw: true })
+
+            if (find === undefined || find === null) {
+                await users.create({
+                    username: fullName,
+                    password: 'oauth',
+                    email: email,
+                    verify: 1
+                })
+                message = `akun ${fullName} berhasil dibuat`
+                res.setHeader("token", accessToken)
+            } else {
+                res.status(409).json({ message: "harap masukkan username, username akun google kamu sudah terpakai" })
+            }
+            return res.status(200).json({ message: message })
         } catch (err) {
             console.error(err.message);
         }
