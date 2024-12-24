5706a954b7df1ed39a7f07af9241ef26589591a7
scorpiontai <scorpiontai22@outlook.com>
2024-12-24 07:14:44 +0700

    added BeforeInit service class in one file + add full functional for recoveryPwAndSendVerifEmail

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,14 +14,69 @@ import axios from 'axios';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class UsersService {
-    constructor(private readonly randomCode: RandomcodeService,
+export class BeforeInit {
+    constructor(private readonly jwt: JwtService,
+        private readonly randomCode: RandomcodeService,
         private readonly mailserv: NodemailerService,
         private readonly redis: RedisService,
         private readonly task: TaskService,
         private readonly enc: EncService,
-        private readonly jwt: JwtService
     ) { }
+    async randomUserName(originName: string): Promise<any> { //random user name 
+        try {
+            let randomFour: any = randomUUID()
+            let randomFourLength: any = Math.floor(randomFour.length / 2)
+            const fullName = `${originName}_${randomFourLength}`
+            return fullName
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    async areVerify(action: string,): Promise<any> {
+        try {
+            let find = action.includes("@") ? await users.findOne({
+                where: {
+                    email: action
+                }
+            }) : await users.findOne({
+                where: {
+                    username: action
+                }
+            })
+
+            return find.verify === 1 ? true : false
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+    async decodeToken(token: any): Promise<any> {
+        try {
+            if (await this.jwt.verifyAsync(token)) {
+                token = await this.jwt.decode(token)
+                let userName = token.userName
+                return userName
+            }
+
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+}
+
+@Injectable()
+export class UsersService {
+    constructor(private readonly jwt: JwtService,
+        private readonly randomCode: RandomcodeService,
+        private readonly mailserv: NodemailerService,
+        private readonly redis: RedisService,
+        private readonly task: TaskService,
+        private readonly enc: EncService,
+        private readonly BeforeInit: BeforeInit
+    ) { }
+
+
     async signup(username: string, password: string, email: string): Promise<any> {
         try {
             if (!username && !password && !email)
@@ -45,41 +100,31 @@ export class UsersService {
             else
                 await users.create({
                     fullName: username,
+                    username: username,
                     password: await argon2.hash(password),
                     email: email
                 })
 
             //set key and iv enc
             //send header 
-            setTimeout(async () => {
-                let encKey = await this.enc.set()
-                encKey = JSON.stringify(encKey)
-                await axios.get(`http://localhost:3000/oauth/encKey/get`, {
-                    headers: {
-                        'encKey': `${email}: ${encKey}`
-                    }
-                })
-                return { message: true }
-            }, 3000)
+
+            let encKey = await this.enc.set()
+            encKey = JSON.stringify(encKey)
+            await axios.get(`http://localhost:3000/oauth/encKey/get`, {
+                headers: {
+                    'encKey': `${email}: ${encKey}`
+                }
+            })
 
 
-            await this.sendVerifyMessage(email) //send verify
+
+            //send verify 
+            await this.redis.setWithTTL(`${email}:verifyCode`, randomUUID(), 5 * 60 * 1000) //set key payload verify
+            const PayloadGet = await this.redis.get(`${email}:verifyCode`) //get
+            await this.mailserv.sendMessage(email, "verifikasi email", "benarkah ini anda?", `http://localhost:3000/verify/email/${PayloadGet}?email=${email}`) //send
+            this.task.scheduleDeleteFiveMinutes(PayloadGet) //callback delete after 5 mins
             return `sukses untuk membuat akun bernama ${username}, kami telah mengirimlan link verifikasi ke email ${email}`
 
-        } catch (err) {
-            console.error(err.message);
-        }
-    }
-
-    async sendVerifyMessage(target: string): Promise<any> {
-        try {
-            const randomCoded = await this.randomCode.generateRandom()
-            const keyName = `${target}:verifyCode` // key name for redis key
-            await this.redis.setWithTTL(keyName, randomCoded, 5 * 6000)
-            //mail action 
-            await this.mailserv.sendMessage(target, `verifikasi`, `verifikasi link`, `http://localhost:3000/verify/email?code=${randomCoded}&email=${target}`)
-            this.task.scheduleDeleteFiveMinutes(keyName) //callback setTimeout to delete 
-            return `sukses mengirim kode verifikasi`  //delete in 5 minutes
         } catch (err) {
             console.error(err.message);
         }
@@ -101,8 +146,9 @@ export class UsersService {
 
 
             const verif = await argon2.verify(finding.password, password)
+
             if (finding && verif) {
-                let token = this.jwt.sign({ for: "all", username: username })
+                let token = this.jwt.sign({ userName: username })
                 return { mesasge: true, token: token }
             } else {
                 return { message: false }
@@ -113,12 +159,34 @@ export class UsersService {
         }
     }
 
-    async randomUserName(originName: string): Promise<any> { //random user name 
+    async recoveryPwSendToMail(email: string, token: any): Promise<any> {
         try {
-            let randomFour: any = randomUUID()
-            let randomFourLength: any = Math.floor(randomFour.length / 2)
-            const fullName = `${originName}_${randomFourLength}`
-            return fullName
+
+            let userName = await this.BeforeInit.decodeToken(token)
+
+            const findUsername = await users.findOne({
+                where: {
+                    fullName: userName
+                }, raw: true
+            })
+
+
+            const allProcess = await Promise.all([
+                await this.redis.setWithTTL(`recover:${email}`, randomUUID(), 5 * 60 * 1000),
+                await this.mailserv.sendMessage(email, "pesan ini untuk anda", `klik disini untuk memulihkan email `,
+                    `${process.env.MAIN_HOST}/verify/forgotPw/${email}?code=${await this.redis.get(`recover:${email}`)}` //start to send
+                ), this.task.scheduleDeleteFiveMinutes(`recover:${email}`) //callback setTimeout to delete
+                , `sukses mengirim ke ${email}`
+            ]) //processed all operations
+
+            if (findUsername.email === email) {
+                await this.BeforeInit.areVerify(email) ?
+                    allProcess
+                    : "terjadi kesalahan, coba lagi nanti"
+                return allProcess[3]
+            } else {
+                return `email tidak cocok dengan username ${userName}`
+            }
         } catch (err) {
             console.error(err.message);
         }
