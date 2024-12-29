8dbb70dfb5874bbdb0a5287ad94a98c6357f7761
scorpiontai <scorpiontai22@outlook.com>
2024-12-29 17:02:26 +0700

    kafka update emitter

--- src/kafka/kafka.service.ts
+++ src/kafka/kafka.service.ts
@@ -1,25 +1,24 @@
 import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { CommandBus, EventBus } from '@nestjs/cqrs';
 import { ClientKafka, EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
+import { time } from 'console';
+import { askCommandDTO } from 'src/ask/asjk.command-dto';
+import { AskCommand } from 'src/ask/ask-intro.command';
+import { notifEvent } from 'src/notif/DTO/notif-dt0.event';
+import { TimeService } from 'src/time/time.service';
 @Injectable()
 export class KafkaService {
 
     constructor(@Inject("useAskKafka") private readonly target: ClientKafka,
+        private readonly commandBus: CommandBus,
+        private readonly eventBus: EventBus,
+        private readonly timeServ: TimeService
     ) { }
-
-
     async emitter(emit: string, message: Object): Promise<any> {
         try {
             this.target.emit(emit, message)
             Logger.debug("success emitter")
 
-        } catch (err) {
-            console.error(err.message);
-        }
-    }
-    @EventPattern("useAsk")
-    async receiver(@Payload() message: Object): Promise<any> {
-        try {
-            
         } catch (err) {
             console.error(err.message);
         }
