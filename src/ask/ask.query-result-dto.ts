83935f6220b176d9a34e26c051d9d82e729acbf3
scorpiontai <scorpiontai22@outlook.com>
2024-12-31 00:34:06 +0700

    udpate dto ask query result

--- src/ask/ask.query-result-dto.ts
+++ src/ask/ask.query-result-dto.ts
@@ -2,10 +2,6 @@ export class askQueryResult {
     constructor(
         public readonly userID: number,
         public readonly questionTotal: string[],
-        public readonly answerTotal: string[],
-        public readonly conclusion: string,
-        public readonly timestart: Date,
-        public readonly timeend: Date,
-        public readonly totaly: string
+        public readonly createdAt: string
     ) { }
 }
