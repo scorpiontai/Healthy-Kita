858952757c5c33f22636ee81e8509cedb8ca688f
scorpiontai <scorpiontai22@outlook.com>
2024-12-30 09:32:30 +0700

    couchbase update

--- src/couchbase/couchbase.service.ts
+++ src/couchbase/couchbase.service.ts
@@ -1,31 +1,32 @@
 import { Injectable, OnModuleInit, Inject, Logger } from '@nestjs/common';
 import * as couchbase from 'couchbase'
 import { EncService } from 'src/enc/enc.service';
+import { TimeService } from 'src/time/time.service';
 @Injectable()
 export class CouchbBaseService {
     constructor(@Inject("couchbase") private readonly couchbase,
-        private readonly encService: EncService) { }
-    async upset(bucketName: string, upsertName: string, content: any, key: any, iv: any): Promise<any> {
+        private readonly encService: EncService,
+        private readonly timeServ: TimeService) { }
+    async upset(userID: number, bucketName: string, upsertName: string, content: any, key?: [number], iv?: [number]): Promise<any> {
         try {
             //before is encryption
-            let arrContent = []
-            const { message, timestamp } = content
-            const encText = await this.encService.enc(key, iv, message)
+
+            let message = key && iv ? await this.encService.enc(key, iv, content) : content
+
 
             const bucket = this.couchbase.bucket(bucketName)
             const coll = bucket.defaultCollection()
 
-            //before is get
-            await coll.upsert(upsertName, { message: message, timestamp: timestamp, sort: "first" })
-            const getData = await this.get("AI", upsertName)
 
-            arrContent.push(getData.content, { message: message, timestamp: timestamp, sort: 'first' })
-            await coll.upsert(upsertName, { arrContent })
+            await coll.upsert(`${upsertName}`, { message: message, timestamp: await this.timeServ.localeString() })
             return "success to upsert"
+
         } catch (err) {
             console.error(err.message);
         }
     }
+
+
     async get(bucketName: string, upsertName?: string): Promise<any> {
         try {
             const bucket = this.couchbase.bucket(bucketName)
@@ -35,6 +36,8 @@ export class CouchbBaseService {
             console.error(err.message);
         }
     }
+
+
     async update(bucketName: string, upsertName: string, content: JSON): Promise<any> {
         try {
             const bucket = this.couchbase.bucket(bucketName)
@@ -45,6 +48,8 @@ export class CouchbBaseService {
             console.error(err.message);
         }
     }
+
+
     async del(bucketName: string, upsertName: string): Promise<any> {
         try {
             const bucket = this.couchbase.bucket(bucketName)
