6a263b4ca367b6c17574b81dc9335f14d9a44e37
scorpiontai <scorpiontai22@outlook.com>
2025-01-01 15:01:44 +0700

    gemini update prompt function to calculation quest result

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,40 +1,105 @@
-import { Injectable } from '@nestjs/common';
-import * as dotenv from 'dotenv'
-import { resolve } from 'path'
-import { GoogleGenerativeAI } from '@google/generative-ai'
-dotenv.config({ path: resolve('./src/.env') });
+import { Injectable, Logger } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import { resolve } from 'path';
 import axios from 'axios';
+import { GoogleGenerativeAI } from "@google/generative-ai"
+import { SystemGateway } from '../system/system.gateway';
+import { rejects } from 'assert';
+dotenv.config({ path: resolve('./src/.env') });
 
 @Injectable()
 export class GeminiService {
-    private key: string
-    private api: string
+    private genAi: any;
+    private model: any;
 
-    constructor() {
-        this.api = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_AI}`
+    constructor(private readonly systemGatewaySocket: SystemGateway) {
+        this.genAi = new GoogleGenerativeAI(process.env.GEMINI_AI);
+        this.model = this.genAi.getGenerativeModel({ model: "gemini-1.5-flash" });
     }
 
-    async sender(question: string): Promise<any> {
+    async sender(name: string, age: number, intensActivity: number): Promise<any> {
         try {
-            const data = {
-                contents: [
-                    {
-                        parts: [
-                            { text: question }
-                        ]
-                    }
-                ]
-            };
-            const prompt = await axios.post(this.api, data, {
-                headers: {
-                    'Content-Type': 'application/json'
+            let prompting = `Hai, saya adalah ${name} saat ini saya berumur ${age}. Saya memiliki intensitas aktvitas fisik ${intensActivity} kali bergereak aktif dalam seminggu
+            saat ini saya ingin mendapatkan 10 soal untuk memantau kesehatan mental saya, berikan saya dalam format array, ingat ya hanya pertanyaan aja tanpa ada opsi jawaban
+            jadai cuman judul pertanyaan aja dalam array urut per index`
+
+            let result: any = await this.model.generateContent(prompting)
+
+            const allResults: any = new Promise(async (resolve, reject) => {
+                try {
+                    resolve(result.response.text())
+                } catch (error) {
+                    reject(error.message)
+                    console.error(error.message)
                 }
             })
 
-            const content = prompt.data.candidates[0]?.content;
-            console.debug('content', content.parts)
-        } catch (err) {
-            console.error(err.message);
+            return allResults
+        } catch (error) {
+            console.error(error.message)
         }
     }
+
+    async sendInUseCache(name: string, age: number, payloadCache: [string]): Promise<any> {
+        try {
+            let prompting = `Hai, saya adalah ${name} saat ini saya berumur ${age}.
+            buatkan saya pertanyaan yang lain selain dari pertanyaan berikut untuk pertanyaan seputar kesehatan mental saya, pertanyaan
+            harus berbeda dari daftar ini:
+            ${payloadCache} tapi saling berkaitan
+            
+            |terimakasih!`
+
+            let result: any = await this.model.generateContent(prompting)
+
+            const allResults: any = new Promise(async (resolve, reject) => {
+                try {
+                    resolve(result.response.text())
+                } catch (error) {
+                    reject(error.message)
+                    console.error(error.message)
+                }
+            })
+
+            return allResults
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
+
+    async calculationQuest(payload: any): Promise<any> {
+        try {
+            let prompting = `Anda sebelum-sebelumnya sudah berbaik hati ke saya memberikan pertanyaan untuk nanti asya jawab sebagai pengukuran
+            tes kesehatan mental saya, maka dari itu ini adalah apa yang saya punya, tolonh analisiskan ini audit kesehatan mental berdasarkan tingkatan/range
+            dengan masing-masing pertanyaan ini: 
+            
+            ${payload} || setelah menganalisa harap laporkan semua hasil analitik anda secara singkat dan potensi penyakit apa saja yang datang 
+            dari masalah mental saya. Tapi bila hasil analitiknya malah tidak ada masalah mental berikan saya pujian dan tips untuk mempertahankan semua ini
+            
+            saya mau hasil analisis di deskripsikan saja, tidak usah bertele tele setelah di deskripsikan masalah saya bila ada tapi bila gak langsung deskripsikan pesan moral dan nasehat
+            kelompokkan hasil analitik dalam format seperti ini
+
+            [massalah mental]
+            [kaitannya dengan kesehatan fisik]
+            [rekomendasi]   
+
+            Kirim balasan dalam bentuk response JSON alih alih teks
+            `
+
+            let result: any = await this.model.generateContent(prompting)
+
+            const allResults: any = new Promise(async (resolve, reject) => {
+                try {
+                    resolve(result.response.text())
+                } catch (error) {
+                    reject(error.message)
+                    console.error(error.message)
+                }
+            })
+
+            return allResults
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
+
 }
