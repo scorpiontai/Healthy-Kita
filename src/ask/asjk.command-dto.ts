9cccb71adf16546efba4c551b053e0be052cc551
scorpiontai <scorpiontai22@outlook.com>
2025-01-02 16:33:42 +0700

    change command dto

--- src/ask/asjk.command-dto.ts
+++ src/ask/asjk.command-dto.ts
@@ -2,6 +2,9 @@ export class askCommandDTO {
     constructor(
         public readonly userID: number,
         public readonly encKey: [number],
-        public readonly ivKey: [number]
+        public readonly ivKey: [number],
+        public readonly fullName: string,
+        public readonly tall: any,
+        public readonly weight: any
     ) { }
 }
