f99da6af20d925a8c423b3a9c38ff89eb9392095
scorpiontai <scorpiontai22@outlook.com>
2025-01-04 04:44:02 +0700

    schema for structure meta (share link, description and etc)

--- src/mongoose/aischema/schema/intro.schema.ts
+++ src/mongoose/aischema/schema/intro.schema.ts
@@ -1,38 +1,23 @@
 
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { randomUUID } from 'crypto';
 import { HydratedDocument } from 'mongoose';
-
 export type IntroDocument = HydratedDocument<Intro>;
 
 @Schema()
 export class Intro {
-
     @Prop({ required: true, type: Number })
     userID: number
 
-    @Prop({ required: false, type: String })
-    fullName: string
+    @Prop({ required: true, type: String, default: randomUUID() })
+    name: string
 
-    @Prop({ required: false, type: String })
-    question: string
-
-    @Prop({ required: true, type: Number })
-    activityIntens: true
-
-    @Prop({ required: true, type: Number })
-    age: number
-
-    @Prop({ required: true, type: Number })
-    weight: number
-
-    @Prop({ required: true, type: Number })
-    tal: number
-
-    @Prop({ required: true, type: String })
-    timestamp: string
+    @Prop({ required: true, type: String, default: new Date().toLocaleString() })
+    time: string
 
     @Prop({ required: true, type: Boolean, default: false })
-    cachingQuestion: boolean
+    publish: boolean
+
 }
 
 export const IntroSchema = SchemaFactory.createForClass(Intro);
