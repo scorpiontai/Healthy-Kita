87257ae6b44aed6cbf13567f9f681503fde7a9d9
scorpiontai <scorpiontai22@outlook.com>
2025-01-03 21:27:10 +0700

    calculation service event on super

--- src/submitted-handler/calculation.service.ts
+++ src/submitted-handler/calculation.service.ts
@@ -10,17 +10,19 @@ import Redlock from 'redlock';
 import { AnswerCommand } from "src/answer/DTO/answer-command.dto";
 import { AuditHistoryService } from "./audit-histrory/audit-history.service";
 import { AuditHisotryEvent } from "./DTO/audit-history.event.dto";
+import { EventEmitter2 } from "@nestjs/event-emitter";
 
 @EventsHandler(CalculationEvent)
 export class CalculationService implements IEventHandler<CalculationEvent> {
     constructor(
         private readonly geminiServ: GeminiService,
         private readonly redisServ: RedisService,
-        private readonly eventBus: EventBus
+        private readonly eventBus: EventBus,
+        private readonly eventEmitter: EventEmitter2
     ) { }
     async handle(event: CalculationEvent) {
         try {
-            let { userIDEnc } = event
+            let { userIDEnc, encKey, ivKey } = event
             let get = await this.redisServ.unlock(`answer:${userIDEnc}`)
             get = JSON.parse(get)
 
@@ -29,12 +31,21 @@ export class CalculationService implements IEventHandler<CalculationEvent> {
             let calculationMessage = calculation.message.replace(/\*/g, '')
             let calculationUserID = calculation.userIDEnc
 
-            let auditHistoryEvent =
-                await this.eventBus.publish(
-                    new AuditHisotryEvent(calculationUserID,
-                        calculationMessage))
+           
+            await this.redisServ.lock(`result_socket:${calculationUserID}`, {
+                userIDD: userIDEnc,
+                content: userIDEnc
+            })  
 
-       
+            await this.eventEmitter.emitAsync("result_socket", { userID: calculationUserID,
+                encKey:encKey,ivKey:ivKey
+             })
+
+
+            await this.eventBus.publish(
+                new AuditHisotryEvent(calculationUserID,
+                    calculationMessage))
+
         } catch (error) {
             console.error(error.message)
             return false
