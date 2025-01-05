695bbe64bb4a0735383bbf314c364d0af25125df
scorpiontai <scorpiontai22@outlook.com>
2025-01-05 14:49:25 +0700

    updatee schema mongoose prePublish as Aischema service

--- src/mongoose/aischema/aischema.service.ts
+++ src/mongoose/aischema/aischema.service.ts
@@ -3,56 +3,41 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Intro, IntroDocument } from './schema/intro.schema';
 import { Model } from 'mongoose';
 import { TimeService } from 'src/time/time.service';
+import { RedisService } from 'src/redis/redis.service';
 @Injectable()
 export class AischemaService {
     constructor(@InjectModel(Intro.name) private introAsk: Model<IntroDocument>,
-        private readonly timeServ: TimeService) { }
-    async checkUser(userID: number): Promise<any> {
+        private readonly timeServ: TimeService,
+        private readonly redisServ: RedisService) { }
+
+    async registByDefault(userID: any, content: string): Promise<any> {
         try {
-            const find = await this.introAsk.findOne({ userID: userID }).exec()
-            return find
+            await this.introAsk.create({
+                userID: userID,
+                content: content
+            })
+            return true
         } catch (err) {
             console.error(err.message);
         }
     }
 
-    async registUser(userID: number, fullName: string, activityIntens: number, age: number, weight: number, tal: number): Promise<any> {
+    //this func for public true and added external description
+    async addExternalInfo(userID: string,
+        description: string, publish: boolean): Promise<any> {
         try {
-            const find = await this.checkUser(userID)
-
-            if (!find) {
-                await this.introAsk.create({
-                    userID: userID, fullName: fullName, activityIntens: activityIntens, age: age,
-                    weight: weight, tal: tal, timestamp: await this.timeServ.localeString()
-                })
-                return true
-            } else {
-                return false
-            }
+            await this.introAsk.updateMany({
+                userID: userID
+            }, {
+                $set: {
+                    description: description,
+                    publish: publish
+                }
+            })
+            return true
         } catch (err) {
             console.error(err.message);
             return false
-        }
-    }
-    async setRecommendedAction(recommendedAction: string, userID: number): Promise<any> {
-        try {
-            await this.introAsk.updateOne({ userID: userID }, { $set: { recommendedAction: recommendedAction } })
-        } catch (err) {
-            console.error(err.message);
-        }
-    }
-
-    async shutDown(userID: number): Promise<any> {
-        try {
-            const find = await this.checkUser(userID)
-            if (find) {
-                const deleted = await this.introAsk.deleteOne({ userID: userID })
-                return deleted ? true : false
-            } else {
-                return false
-            }
-        } catch (err) {
-            console.error(err.message);
         }
     }
 }
