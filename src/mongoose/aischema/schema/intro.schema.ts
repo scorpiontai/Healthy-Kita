b23e4b35fa385e3ea13d53358c15cce0c0e05aa5
scorpiontai <scorpiontai22@outlook.com>
2025-01-06 23:01:07 +0700

    updte schema

--- src/mongoose/aischema/schema/intro.schema.ts
+++ src/mongoose/aischema/schema/intro.schema.ts
@@ -1,28 +1,35 @@
-
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { randomUUID } from 'crypto';
 import { HydratedDocument } from 'mongoose';
 export type IntroDocument = HydratedDocument<Intro>;
 
 @Schema()
 export class Intro {
+    //hashed index
     @Prop({ required: true, type: String })
     userID: string
 
-    @Prop({ required: false, type: Date, default: new Date().toISOString() })
-    prePublishTime: Date
+    //index partial by true
+    @Prop({ required: true, type: Number, default: 1 })
+    publish: number
 
-    @Prop({ required: true, type: Boolean, default: false })
-    publish: boolean
-
-    @Prop({ requried: true, type: String, default: randomUUID() })
-    uuid: string
+    //index partial by true
+    @Prop({ required: true, type: Number, default: 1 })
+    commentAllow: number
 
     @Prop({ required: true, type: String })
-    AuditID: string
+    uuid: string
+
+
+
+    @Prop({ required: false, type: Date, default: new Date().toLocaleString() })
+    prePublishTime: Date
 
     @Prop({ required: false, type: String, default: null })
     description: string
+
+    //hash index
+    @Prop({ required: true, type: String })
+    url: string
 }
 
 export const IntroSchema = SchemaFactory.createForClass(Intro);
