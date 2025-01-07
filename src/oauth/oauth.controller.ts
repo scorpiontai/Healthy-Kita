a80c0b9c0a750aed3469974c59cb620e1d106c6e
scorpiontai <scorpiontai22@outlook.com>
2025-01-08 01:33:32 +0700

    o auth added

--- src/oauth/oauth.controller.ts
+++ src/oauth/oauth.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, UseGuards, Get, Post, Req, Res, Logger, Body, Param } from '@nestjs/common';
+import { Controller, UseGuards, Get, Req, Res } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { users } from 'src/models/users.models';
-import { UsersService } from 'src/users/users.service';
 import axios from 'axios'
 import { RedisService } from 'src/redis/redis.service';
 import { EncService } from 'src/enc/enc.service';
+import { Response } from 'express'
+
 @Controller('oauth')
 export class OauthController {
-    constructor(private readonly users: UsersService,
+    constructor(
         private readonly redis: RedisService,
         private readonly enc: EncService,
     ) { }
@@ -21,22 +22,13 @@ export class OauthController {
         }
     }
 
-    async HandlingIfAlreadyHaveToken(email: string): Promise<any> {
+    async HandlingIfAlreadyHaveToken(token: string): Promise<any> {
         try {
-            if (!email)
-                console.error("terjadi kesalahan sistem")
+            const endpointVerify = `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${token}`
 
 
-            const findIfExistsToken = await this.redis.get(`${email}:accessToken`)
-            if (findIfExistsToken) {
-                const endpointVerify = `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${findIfExistsToken}`
-
-
-                const areVerify = await axios.get(endpointVerify)
-                return areVerify.data.verified_email // return true or false
-            } else {
-                return false
-            }
+            const areVerify = await axios.get(endpointVerify)
+            return areVerify.data.verified_email
         } catch (err) {
             console.error(err.message);
         }
@@ -44,77 +36,46 @@ export class OauthController {
 
     @Get("google/callback")
     @UseGuards(AuthGuard('google'))
-    async googleAuthRedirect(@Req() req, @Res() res): Promise<any> {
+    async googleAuthRedirect(@Req() req, @Res() res: Response): Promise<any> {
         try {
-            const { accessToken, firstName, lastName, email, picture } = req.user
-            let message
-            //logic here 
-            // https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=
+            let { accessToken, firstName, lastName, email, picture } = req.user
+            const find = await this.redis.get(`token:${email}`)
 
-            const findIfExistsToken = await this.HandlingIfAlreadyHaveToken(email)
-
-
-
-            if (!findIfExistsToken) {
-                if (!accessToken)
-                    return res.status(400).json({ message: "token tidak ditemukan, harap coba ulangi atau hubungi developer" })
-
-                //jika berhasil, maka buat akun
-                const fullName = `${firstName} ${lastName}`
-                const find: any = await users.findOne({ where: { fullName: fullName, email: email, password: "oauth" }, raw: true })
-
-
-                if (find === undefined || find === null) {
-                    await users.create({
-                        fullName: fullName,
-                        password: 'oauth',
+            lastName = lastName.replace(/\s+/g, '_').toLowerCase()
+            if (!find) {
+                const find = await users.findOne({
+                    where: {
                         email: email,
-                        verify: 1
+                        encInfo: 1,
+                    }, attributes: ['ID']
+                })
+
+                if (!find) {
+
+                    await users.create({
+                        fullName: `${firstName} ${lastName}`,
+                        username: `${lastName}_${new Date().valueOf()}`,
+                        email: email,
+                        pictures: picture,
+                        encInfo: 1
                     })
 
-                    await this.redis.setWithTTL(`${email}:accessToken`, accessToken, 3600)
-                    message = `sukses untuk membuat akun ${fullName}`
-
-                    //set key and iv 
-                    //async load 3 min
-
-                    /*
-                    key and Iv is a private and only user have store in client side, 
-                    you as a backend developer please keep secure and do not see use enc key,
-                    make secure send to client side with another technic such as header send etc
-                    */
-
-                    //this is
-                    setTimeout(async () => {
-                        let encKey = await this.enc.set()
-                        encKey = JSON.stringify(encKey)
-                        await axios.get(`http://localhost:3000/oauth/encKey/get`, {
-                            headers: {
-                                'encKey': `${email}: ${encKey}`
-                            }
-                        })
-                        return { message: true }
-                    }, 3000)
+                    let getKey = await this.enc.set()
+                    const { key, iv } = getKey
 
 
+                    await this.redis.lockForAccses(email, accessToken)
+                    return res.status(200).json({ status: 200, encKey: key, ivKey: iv })
                 } else {
-                    //if acccount hass created, only set redis key store accessToken
-                    await this.redis.setWithTTL(`${email}:accessToken`, accessToken, 3600)
-                    //verify
-                    return findIfExistsToken ? res.status(301).redirect(`http://localhost:8080/dahsboard`) : message = "ups..terjadi error, coba lagi nanti, jika kendala masih berlanjut. Harap hubungi pusat bantuan"
+                    const token = await this.redis.get(`token:${email}`)
+                    const verify = await this.HandlingIfAlreadyHaveToken(token)
+                    return verify ? res.status(200).json({ status: 200 }) : res.status(500).json({ staus: 500 })
                 }
-
             } else {
-                return res.status(301).json({ message: true })
+                const token = await this.redis.get(`token:${email}`)
+                const verify = await this.HandlingIfAlreadyHaveToken(token)
+                return verify ? res.status(200).json({ status: 200 }) : res.status(500).json({ staus: 500 })
             }
-        } catch (err) {
-            console.error(err.message);
-        }
-    }
-
-    @Get("encKey/get")
-    async encKeyGet(@Req() req): Promise<any> {
-        try {
         } catch (err) {
             console.error(err.message);
         }
