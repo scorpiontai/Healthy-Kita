a62eb9a087d215457c8cc7feab84d13d55068063
scorpiontai <scorpiontai22@outlook.com>
2025-01-11 15:33:23 +0700

    add health guard

--- src/health-add/health-add.guard.ts
+++ src/health-add/health-add.guard.ts
@@ -1,36 +1,42 @@
 import { CanActivate, ExecutionContext, Injectable, Logger } from '@nestjs/common';
-import { Request } from 'express'
+import { Request, Response } from 'express'
 import { JwtService } from '@nestjs/jwt';
-import { users } from 'src/models/users.models';
 import { BeforeInit } from 'src/users/users.service';
-import { OauthController } from 'src/oauth/oauth.controller';
+import { oauthService } from 'src/oauth/oauth.controller';
 import { RedisService } from 'src/redis/redis.service';
 
 @Injectable()
 export class HealthAddGuard implements CanActivate {
   constructor(
-    private readonly jwt: JwtService,
-    private readonly Beforenit: BeforeInit,
-    private readonly oauth: OauthController,
+    private readonly oauth: oauthService,
     private readonly redisServ: RedisService
   ) { }
   async canActivate(
     context: ExecutionContext,
   ): Promise<any> {
     try {
-      const ctx = context.switchToHttp()
-      const req: Request = ctx.getRequest()
-      const res: Response = ctx.getResponse()
-      let { tokenUser } = req.cookies
+      const ctx = context.switchToHttp();
+      const req: Request = ctx.getRequest();
+      const res: Response = ctx.getResponse();
 
-      if (tokenUser.includes("ey")) {
-        const decode = await this.Beforenit.decodeToken(tokenUser)
-        let { userID } = decode
 
+      let { tokenUser } = req.cookies;
+
+      if (typeof tokenUser === 'string' && tokenUser.includes("ey")) {
         return true
-      } else{
-        
+      } else if (tokenUser.includes("@")) {
+        const token = await this.redisServ.get(`token:${tokenUser}`)
+        if (token) {
+          console.log("this", !token)
+          const verif = await this.oauth.HandlingIfAlreadyHaveToken(token)
+          return verif.status
+        } else {
+          return res.redirect(`http://localhost:6060/oauth/google/`)
+        }
+      } else {
+        return res.redirect(`http://localhost:6060/oauth/google/`)
       }
+
     } catch (error) {
       console.error(error.message)
     }
