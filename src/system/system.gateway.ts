3469c02a0288f293c9963de7e1d1ca2405587fb9
scorpiontai <scorpiontai22@outlook.com>
2025-01-15 09:47:53 +0700

    system socket gateway

--- src/system/system.gateway.ts
+++ src/system/system.gateway.ts
@@ -5,27 +5,50 @@ import { BeforeInit } from 'src/users/users.service';
 import { Socket, Server } from 'socket.io';
 import { AnswerCommand } from 'src/answer/DTO/answer-command.dto';
 import { EncService } from 'src/enc/enc.service';
-import { Logger, OnModuleDestroy } from '@nestjs/common';
+import { OnModuleDestroy } from '@nestjs/common';
 import { RedisService } from 'src/redis/redis.service';
 import { AskQueryResult } from 'src/ask/ask-query.query.dto';
-import { AnswerQueryRead } from 'src/answer/DTO/answer-query-read.dto';
-import { OauthController } from 'src/oauth/oauth.controller';
+import { oauthService } from 'src/oauth/oauth.controller';
+import { GuardStart } from 'src/ask/guard/guard.start.quiz';
+import { QuizGuard } from 'src/ask/guard/DTO/QuizGuard..dto';
 @WebSocketGateway({
   cors: {
     origin: "*",
   }
 })
 export class SystemGateway implements OnModuleDestroy {
+  private readonly map = new Map()
   constructor(private readonly commandBus: CommandBus,
     private readonly BeforeInit: BeforeInit,
     private readonly queryBus: QueryBus,
     private readonly encServ: EncService,
     private readonly redisServ: RedisService,
-    private readonly oauth: OauthController
+    private readonly oauth: oauthService
   ) { }
+
   @WebSocketServer()
   server: Server
   client: Socket
+
+  async cacheJoin(key, value): Promise<any> {
+    try {
+      this.map.set(key, value)
+      setTimeout(() => {
+        this.map.delete(key)
+      }, 10 * 60 * 1000)
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  async getCacheJoin(key): Promise<any> {
+    try {
+      const find = await this.map.get(key)
+      return find
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
 
   @SubscribeMessage("join")
   async set(client: Socket, payload: any): Promise<any> {
@@ -33,21 +56,18 @@ export class SystemGateway implements OnModuleDestroy {
       let { tokenUser, encKey, ivKey } = payload.init
       let userID
       let encID
-      if (!tokenUser.includes("@")) {
-        tokenUser = await this.BeforeInit.decodeToken(tokenUser)
-        userID = tokenUser.userID
-      } else {
-        const email = await this.redisServ.get(`token:${tokenUser}`)
-        const oauth = await this.oauth.HandlingIfAlreadyHaveToken(email)
-        const { ID } = oauth
-        userID = ID
 
-      }
+      tokenUser = await this.BeforeInit.decodeToken(tokenUser)
+      userID = tokenUser.userID
+
       encID = await this.encServ.enc(encKey, ivKey, userID.toString())
       client.join(encID)
 
 
-      return { status: true }
+      const guardQuery = await this.queryBus.execute(
+        new QuizGuard(tokenUser)
+      )
+      this.server.to(encID).emit("guard", { status: guardQuery })
     } catch (err) {
       console.error(err.message);
     }
@@ -61,34 +81,30 @@ export class SystemGateway implements OnModuleDestroy {
     let encID
 
     try {
-      if (!tokenUser.includes("@")) {
-        tokenUser = await this.BeforeInit.decodeToken(tokenUser)
-        userID = tokenUser.userID
-      } else {
-        const email = await this.redisServ.get(`token:${tokenUser}`)
-        const oauth = await this.oauth.HandlingIfAlreadyHaveToken(email)
-        const { ID } = oauth
-        userID = ID
-      }
-
+      tokenUser = await this.BeforeInit.decodeToken(tokenUser)
+      userID = tokenUser.userID
       encID = await this.encServ.enc(encKey, ivKey, userID.toString())
+
       const check = await this.redisServ.get(`start:${encID}`)
 
       if (!check) {
+        //start lock
+        await this.redisServ.set(`start:${encID}`, 1)
         let commandBus =
           await this.commandBus.execute(
             new askCommandDTO(
               userID,
               encKey,
               ivKey))
-
+        console.debug("start", commandBus.client)
         this.server.to(commandBus.client)
           .emit("responseStart", {
             message: commandBus.message
           })
       } else {
+        console.debug(encID)
         this.server.to(encID).emit("responseStart",
-          { status: 409 }
+          { status: 409, previous: true }
         )
       }
     } catch (err) {
@@ -103,16 +119,9 @@ export class SystemGateway implements OnModuleDestroy {
       let userID
       let encID
 
-      if (!tokenUser.includes("@")) {
-        tokenUser = await this.BeforeInit.decodeToken(tokenUser)
-        userID = tokenUser.userID
-      } else {
-        const email = await this.redisServ.get(`token:${tokenUser}`)
-        const oauth = await this.oauth.HandlingIfAlreadyHaveToken(email)
-        const { ID } = oauth
-        userID = ID
-        console.debug("user id is", userID)
-      }
+
+      tokenUser = await this.BeforeInit.decodeToken(tokenUser)
+      userID = tokenUser.userID
       encID = await this.encServ.enc(encKey, ivKey, userID.toString())
       let question = payload.answer
 
@@ -157,6 +166,7 @@ export class SystemGateway implements OnModuleDestroy {
 
       if (check) {
         await this.redisServ.del(`start:${userIDEnc}`)
+        //deleted for lock validation
       }
 
       this.server.to(userIDEnc).emit("TheResult",
@@ -166,40 +176,6 @@ export class SystemGateway implements OnModuleDestroy {
           }, next: true
         }
       )
-    } catch (err) {
-      console.error(err.message);
-    }
-  }
-
-  @SubscribeMessage("notifJoin")
-  async notifJoin(client: Socket, payload: any): Promise<any> {
-    try {
-      let { tokenUser, encKey, ivKey } = payload.init
-      let userID
-      let encID
-      if (!tokenUser.includes("@")) {
-        tokenUser = await this.BeforeInit.decodeToken(tokenUser)
-        userID = tokenUser.userID
-      } else {
-        const email = await this.redisServ.get(`token:${tokenUser}`)
-        const oauth = await this.oauth.HandlingIfAlreadyHaveToken(email)
-        const { ID } = oauth
-        userID = ID
-      }
-      encID = await this.encServ.enc(encKey, ivKey, userID.toString())
-
-      client.join(`notifications:${userID}`)
-      console.debug("userid is", userID)
-    } catch (err) {
-      console.error(err.message);
-    }
-  }
-
-  async notifReceivMessage(userID: number, messages: any): Promise<any> {
-    try {
-      this.server.to(`notifications:${userID}`).emit("notifReceiv", {
-        message: messages
-      })
     } catch (err) {
       console.error(err.message);
     }
