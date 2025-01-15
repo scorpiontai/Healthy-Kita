ec9a96e981e93fd8ea96689de5b744be01d6b621
scorpiontai <scorpiontai22@outlook.com>
2025-01-15 09:53:56 +0700

    added app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,20 @@
-import { Controller, Get, Post, Res, Req, Body, UseGuards, Query, Logger } from '@nestjs/common';
+import { Controller, Get, Post, Res, Req, Body } from '@nestjs/common';
 import { Response, Request } from 'express';
 import { BeforeInit, UsersService } from './users/users.service';
 import { Users } from './users/DTO/users.dto';
 import { EncService } from './enc/enc.service';
 import { users } from './models/users.models';
-import { HealthAddGuard } from './health-add/health-add.guard'
+import { RedisService } from './redis/redis.service';
+import { KafkaService } from './kafka/kafka.service';
+
 @Controller("api")
-/*
-
-This API for user bassic credentials and security management
-only rest, guard for authorization, cookie and heders for resource
-thank you 
-
-*/
 export class AppController {
   constructor(
     private readonly userService: UsersService,
     private readonly enc: EncService,
-    private readonly BeforeInit: BeforeInit
+    private readonly BeforeInit: BeforeInit,
+    private readonly redisServ: RedisService,
+    private readonly kafkaServ: KafkaService,
   ) { }
 
 
@@ -48,8 +45,28 @@ export class AppController {
           res.status(200).cookie("tokenUser", loginned.token, { httpOnly: true, secure: true, maxAge: 10 * 60 * 1000, sameSite: 'none' })
 
         //set enc key and enc iv
-        const allEncAndIV = await this.enc.set()
-        res.status(200).json({ message: loginned.message, encKeyIv: allEncAndIV })
+        const encInfo = await users.findOne({
+          where: {
+            encInfo: 0
+          },
+          attributes: ['encInfo'], raw: true
+        })
+
+        if (encInfo.encInfo === 0) {
+          await users.update({
+            encInfo: 1
+          }, {
+            where: {
+              encInfo: 0,
+              username: username
+            }
+          })
+          const allEncAndIV = await this.enc.set()
+          res.status(200).json({ message: loginned.message, encKeyIv: allEncAndIV })
+        } else {
+          res.status(200).json({ message: loginned.message })
+        }
+
       } else {
         return ({ message: "gagal untuk mencoba login, mungkin anda belum terverifikasi" })
       }
@@ -101,57 +118,19 @@ export class AppController {
   }
 
   @Get("get/credentials")
-  async getCredentials(@Req() req: Request): Promise<any> {
+  async getCredentials(@Req() req: Request, @Res() res: Response): Promise<any> {
     try {
       const { tokenUser } = req.cookies
-      return { tokenUser: tokenUser }
-    } catch (err) {
-      console.error(err.message);
-    }
-  }
 
+      if (tokenUser.includes("@")) {
+        const check = await this.redisServ.get(`token:${tokenUser}`)
 
-  @UseGuards(HealthAddGuard)
-  @Get("input/health/info")
-  // curl http://localhost:6060/api/input/health/info  -b "tokenUser=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6InVzZXJUZXN0IiwiZW1haWwiOiJtaWtoYWVsamhvbjIyQGdtYWlsLmNvbSIsImlhdCI6MTczNjM3NTU2NywiZXhwIjoxNzM3MjM5NTY3fQ.j4CtNrMbwkO4Sy1ZXmeBf4RXGSnitLFC53BZqmsHoOc"
-  async heatlthAdd(@Body() body: any, @Req() req: Request): Promise<any> {
-    const { birthdate, intensActivity, weight, tall } = body
-    let { tokenUser } = req.cookies
-    tokenUser = await this.BeforeInit.decodeToken(tokenUser)
-    let { userID } = tokenUser
-    try {
-      /*
-      Logger.debug("user id health is ",
-        userID
-      )
-
-      await users.update({
-        birthdate: birthdate,
-        intensActivityWeek: intensActivity,
-        weight: weight,
-        tall: tall
-      }, {
-        where: {
-          ID: userID
+        if (!check) {
+          return res.status(200).json({ redirect: `http://localhost:6060/oauth/google` })
+        } else {
+          return res.status(200).json({ tokenUser: tokenUser, redireect: false })
         }
-      })
-        */
-      return {
-        status: 200,
-        message: `sukses untuk mengedit`
       }
-    } catch (err) {
-      console.error(err.message);
-    }
-  }
-
-  @Post("oauth/get/credentials")
-  async oauth(@Req() req: Request, @Body() body: any): Promise<any> {
-    try {
-      const { accessToken } = req.headers
-      const { encKey, ivKey } = body
-
-      
     } catch (err) {
       console.error(err.message);
     }
